7bb6d557a8d94cd50d6f4a6486fe4cde12e3faa2
Den <denissvet.code@gmail.com>
2017-11-06 16:40:25 +0200

    modified acording to last AngularFire2 version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+export class Message {
+message: string;
+name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  itemsCol: AngularFireList<Message>;
+  items: Observable<Message[]>;
+  name: any;
+  msgVal: string = '';
+  afList: any ;
+  constructor(public db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    // this.itemsCol = db.list('/messages', ref => ref.orderByChild('name'));
+    this.itemsCol = db.list('/messages');
+    this.items = this.itemsCol.valueChanges();
+    afAuth.authState.subscribe(
+      auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    }
+  );
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+  chatSend(theirMessage: string) {
+    const msg = new Message();
+    msg.message = theirMessage;
+    msg.name = this.name.displayName;
+    this.itemsCol.push(msg);
+    this.msgVal = '';
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyABDCGLIXgnzO1CQ7YPJQGZMbfTBd8jdps',
+  authDomain: 'chatapper-250da.firebaseapp.com',
+  databaseURL: 'https://chatapper-250da.firebaseio.com',
+  projectId: 'chatapper-250da',
+  storageBucket: '',
+  messagingSenderId: '838858243256'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
